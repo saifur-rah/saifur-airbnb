08323596a4eb4a22dacd5a653e9632aedc94dc97
unknown <saifurrahman8051@gmail.comgit config --global user.email saifurrahman8051@gmail.com>
2022-02-08 05:19:40 +0530

    day 2

--- components/Header.js
+++ components/Header.js
@@ -87,6 +87,7 @@ function Header({placeholder}) {
            minDate={new Date()}
            rangeColors={["#FD5B61"]}
            onChange={handleSelect}
+         
            />
            <div className='flex items-center border-b mb-4'>
              <h2 className='text-2xl flex-grow font-semibold'>Number of Guests</h2>
