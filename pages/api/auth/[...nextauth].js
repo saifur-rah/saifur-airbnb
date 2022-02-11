b4b69ebdc88aad175987170710ae40c45806e55d
unknown <saifurrahman8051@gmail.comgit config --global user.email saifurrahman8051@gmail.com>
2022-02-11 14:32:25 +0530

    dekho kya hota hai

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -44,7 +44,27 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+      authorizationUrl:
+        'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
     }),
     // ...add more providers here
   ],
-})
+  jwt: {
+    encryption: true,
+  },
+  secret: process.env.SECRET,
+  callbacks: {
+    async jwt(token, account) {
+      if (account?.accessToken) {
+        token.accessToken = account.accessToken;
+      }
+      return token;
+    },
+    redirect: async (url, _baseUrl) => {
+      if (url === '/profile') {
+        return Promise.resolve('/');
+      }
+      return Promise.resolve('/');
+    },
+  },
+});
